f6ab02e5879cc55582730969b45e61e4e4caba6e
jrconlin <jconlin+git@mozilla.com>
2018-06-26 14:38:52 -0700

    bug: Set the vapid expiration to 5 minutes, because clock skew.

--- vapid.js
+++ vapid.js
@@ -36,7 +36,7 @@ class VapidToken {
         }
         if (! exp) {
             // Set expry to be 24 hours from now.
-            exp = (Date.now() * .001) + 86400
+            exp = (Date.now() * .001) + 300
         }
         this._claims["exp"] = exp;
         this.lang = {
@@ -152,7 +152,7 @@ class VapidToken {
             throw new Error(this.lang.errs.ERR_NO_KEYS);
         }
         if (!claims.hasOwnProperty("exp")) {
-            claims.exp = parseInt(Date.now() * .001) + 86400;
+            claims.exp = parseInt(Date.now() * .001) + 300;
         }
         if (!claims.hasOwnProperty("sub")) {
             throw new Error(this.lang.errs.ERR_CLAIM_MIS, "sub");
